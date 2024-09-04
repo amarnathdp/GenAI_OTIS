8e83a55142e37c0ca4d807c1948c48d5e3a66e43
amarnathdp <118151760+amarnathdp@users.noreply.github.com>
2024-09-05 02:00:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 // app.use(cors(corsOptions));
 
 app.use(cors({
-  origin: 'https://66d8aed98e21162578d4341b--elaborate-maamoul-e39536.netlify.app', 
+  origin: '*', 
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true 
@@ -27,7 +27,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
   
-    res.setHeader('Access-Control-Allow-Origin', 'https://66d8aed98e21162578d4341b--elaborate-maamoul-e39536.netlify.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
